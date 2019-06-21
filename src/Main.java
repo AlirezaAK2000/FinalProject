7ab87af6e695060e9d5ed51bc889c5eb9eacba74
amir <askaria079@gmail.com>
2019-06-21 10:44:58 +0430

    hi

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         Manager manager = new Manager();
+        System.out.println("hi");
     }
 }
